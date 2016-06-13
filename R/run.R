6d8f2cb08884cf94ff5eef545facf67d36c841b7
tknopik <tomasz.knopik@gmail.com>
2016-06-13 21:56:11 +0200

    added package

---
+++ R/run.R
@@ -0,0 +1,14 @@
+#' The only one function of package
+#' 
+#' Runs Shiny app.
+#'
+#' @export
+run <- function() {
+  
+  appDir <- system.file("shiny", "usosstats", package = "usosstats")
+  if (appDir == "") {
+    stop("Nie znaleziono aplikacji. Spróbuj zainstalować pakiet ponownie.", call. = FALSE)
+  }
+  
+  shiny::runApp(appDir, display.mode = "normal")
+}
